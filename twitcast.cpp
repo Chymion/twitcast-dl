3f500fe2b1dbc1b7802842f49c0d7a2378845c59
Loryon <33351293+Chymion@users.noreply.github.com>
2022-03-30 13:11:07 +0200

    Fixed spm bug

--- twitcast.cpp
+++ twitcast.cpp
@@ -21,12 +21,8 @@ int main()
 	std::cout << "====================" << std::endl;
 	std::cout << "TWITCASTING DOWNLOAD" << std::endl;
 	std::cout << "====================" << std::endl;
-	if (!access("C:/Program Files (x86)/ffmpeg/ffmpeg.exe", F_OK) == 0) {
-		std::cout << "!!!!ERROR!!!!\nffmpeg.exe not found!\nPlease, place it in C:/Program Files (x86)/ffmpeg/ffmpeg.exe \n";
-		std::cin.get();
-		exit(0);
-	}
-	else {
+	if (access("C:/Program Files (x86)/ffmpeg/ffmpeg.exe", F_OK) == 0 || access("C:/Program Files/ffmpeg/ffmpeg.exe", F_OK) == 0) {
+		
 		std::cout << "Paste this link in your browser's console \n\nlet a=[];for(let _ of JSON.parse(document.querySelector(\"video\")[\"dataset\"][\"moviePlaylist\"])[2])a.push(_.source?.url);console.log(a.join(\"\\n\"))\n\n";
 		std::cout << "Enter the url of the video: ";
 		std::cin >> s;
@@ -37,6 +33,19 @@ int main()
 		std::cout << "Enter the user-agent of your browser: ";
 		char input[200] = { 0 };
 		std::cin.getline(input, 200);
+
+		if (s.find("&spm=1") != std::string::npos)
+		{
+			
+			int pos = s.find("&spm=1");
+			std::string ss = "&spm=1";
+			if (pos != std::string::npos)
+			{
+				s = s.erase(pos, ss.length());
+				
+			}
+			
+		}
 
 		std::string g;
 		for (int i = 0; i < s.length(); i++)
@@ -60,17 +69,36 @@ int main()
 			if (input[i] == '\0') break;
 			ressed.push_back(input[i]);
 		}
-		std::string request = "\"\"%ProgramFiles(x86)%/ffmpeg/ffmpeg.exe\" -protocol_whitelist file,http,https,tcp,tls,crypto -user_agent \"" + ressed + "\" -headers \"Origin: https://twitcasting.tv\" -i " + s + " -c copy " + nameOut + ".mkv \"";
-		system(request.c_str());
+		if (access("C:/Program Files/ffmpeg/ffmpeg.exe", F_OK) == 0) {
+			std::cout << "You have to put ffmpeg.exe in Program Files x86, not Program Files..." << std::endl;
+			std::cin.get();
+			exit(0);
+			//std::string request = "\"\"%ProgramFiles%/ffmpeg/ffmpeg.exe\" -protocol_whitelist file,http,https,tcp,tls,crypto -user_agent \"" + ressed + "\" -headers \"Origin: https://twitcasting.tv\" -i " + s + " -c copy " + nameOut + ".mkv \"";
+			//system(request.c_str());
+			//std::cin.get();
+		}
+		else if (access("C:/Program Files (x86)/ffmpeg/ffmpeg.exe", F_OK) == 0) {
+			std::string request = "\"\"%ProgramFiles(x86)%/ffmpeg/ffmpeg.exe\" -protocol_whitelist file,http,https,tcp,tls,crypto -user_agent \"" + ressed + "\" -headers \"Origin: https://twitcasting.tv\" -i " + s + " -c copy " + nameOut + ".mkv \"";
+			system(request.c_str());
+			std::cin.get();
+		}
+		
 		system("cls");
 		std::cout << "===================\nDownload completed!\n===================\n";
 		std::cout << "Do you want an mp4 version? [y/n]: ";
 		std::string resp;
 		std::cin >> resp;
 		std::cin.ignore();
-		if (resp == "y" ||  resp == "Y") {
-			std::string req = "\"\"%ProgramFiles(x86)%/ffmpeg/ffmpeg.exe\" -i " + nameOut + ".mkv -codec copy " + nameOut + ".mp4\"";
-			system(req.c_str());
+		if (resp == "y" || resp == "Y") {
+			if (access("C:/Program Files/ffmpeg/ffmpeg.exe", F_OK) == 0) {
+				std::string req = "\"\"%ProgramFiles%/ffmpeg/ffmpeg.exe\" -i " + nameOut + ".mkv -codec copy " + nameOut + ".mp4\"";
+				system(req.c_str());
+			}
+			else if (access("C:/Program Files (x86)/ffmpeg/ffmpeg.exe", F_OK) == 0) {
+				std::string req = "\"\"%ProgramFiles(x86)%/ffmpeg/ffmpeg.exe\" -i " + nameOut + ".mkv -codec copy " + nameOut + ".mp4\"";
+				system(req.c_str());
+			}
+			
 			std::string dest = "del " + nameOut + ".mkv";
 			system(dest.c_str());
 			system("cls");
@@ -100,7 +128,11 @@ int main()
 				exit(0);
 			}
 		}
-
+	}
+	else {
+		std::cout << "!!!!ERROR!!!!\nffmpeg.exe not found!\nPlease, place it in C:/Program Files (x86)/ffmpeg/ffmpeg.exe or C:/Program Files/ffmpeg/ffmpeg.exe \n";
+		std::cin.get();
+		exit(0);
 	}
 #else
 	begin:
